d6b83d6f50a0253298702cf6d63f567fd06034e5
MykhailoVobolis <mvobolis@gmail.com>
2025-02-12 06:44:32 +0200

    added validation form data

--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import validator from 'validator';
+import Inputmask from 'inputmask';
 
 const registerForm = document.querySelector('form');
 const localStorageKey = 'feedback-form-state';
@@ -24,38 +26,83 @@ function recordLocalStorage(event) {
 
 registerForm.addEventListener('submit', sendingData);
 
+function setError(inputElement, message) {
+  const errorMessageElement = inputElement.nextElementSibling;
+  errorMessageElement.textContent = message;
+  errorMessageElement.classList.add('show');
+}
+
+function resetErrors() {
+  const inputs = registerForm.querySelectorAll('.form-input');
+  inputs.forEach(input => {
+    const errorMessageElement = input.nextElementSibling;
+    errorMessageElement.classList.remove('show');
+  });
+}
+
+const phoneInput = document.querySelector('input[name="phone"]');
+const im = new Inputmask('+380 (99) 999 99 99');
+im.mask(phoneInput);
+
 async function sendingData(event) {
   event.preventDefault();
 
+  resetErrors();
+
+  let isValid = true;
+
+  if (!userName.value.trim()) {
+    setError(userName, "Це поле є обов'язковим");
+    isValid = false;
+  }
+
+  if (!userEmail.value.trim()) {
+    setError(userEmail, "Це поле є обов'язковим");
+    isValid = false;
+  } else if (!validator.isEmail(userEmail.value)) {
+    setError(userEmail, 'Невірний формат email');
+    isValid = false;
+  }
+
+  if (!userPhone.value.trim()) {
+    setError(userPhone, "Це поле є обов'язковим");
+    isValid = false;
+  } else if (
+    !validator.matches(userPhone.value, /^\+380 \(\d{2}\) \d{3} \d{2} \d{2}$/)
+  ) {
+    setError(userPhone, 'Невірний формат телефону');
+    isValid = false;
+  }
+
   if (!userPrivacy.checked) {
-    return alert(
-      'Будь ласка, погодьтеся з політикою конфіденційності та умовами користування послугами'
-    );
+    setError(userPrivacy, 'Потрібно погодитись з політикою конфіденційності');
+    isValid = false;
   }
 
-  if (!userName.value || !userEmail.value || !userPhone.value) {
-    return alert('Будь ласка, заповніть всі поля форми');
-  }
+  if (isValid) {
+    const formData = {
+      name: userName.value.trim(),
+      email: userEmail.value.trim(),
+      phone: userPhone.value.trim(),
+    };
 
-  const formData = {
-    name: userName.value.trim(),
-    email: userEmail.value.trim(),
-    phone: userPhone.value.trim(),
-  };
+    try {
+      // Надсилання POST-запиту на неіснуючий ендпоінт
+      const response = await axios.post(
+        'https://example.com/register',
+        formData
+      );
 
-  try {
-    // Надсилання POST-запиту на неіснуючий ендпоінт
-    const response = await axios.post('https://example.com/register', formData);
+      localStorage.removeItem(localStorageKey);
+      registerForm.reset();
 
-    localStorage.removeItem(localStorageKey);
-    registerForm.reset();
+      alert('Дані форми успішно надіслані');
+    } catch (error) {
+      alert('Сталася помилка при відправленні даних форми');
 
-    alert('Дані форми успішно надіслані');
-  } catch (error) {
-    alert('Сталася помилка при відправленні даних форми');
-
-    // Тільки для демонстрації !!!
-    localStorage.removeItem(localStorageKey);
-    registerForm.reset();
+      // Тільки для демонстрації !!!
+      localStorage.removeItem(localStorageKey);
+      registerForm.reset();
+    }
   }
 }

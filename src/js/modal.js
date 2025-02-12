73b7f19dc7198578455797e2939988b951dc69d3
MykhailoVobolis <mvobolis@gmail.com>
2025-02-12 09:41:38 +0200

    cleaned styles

---
+++ src/js/modal.js
@@ -0,0 +1,20 @@
+// const modal = document.querySelector('.modal');
+// const openModalBtn = document.querySelector('.registration-toggle-btn');
+// const closeModalBtn = document.querySelector('.modal-close-btn');
+
+// // Відкрити модалку
+// openModalBtn.addEventListener('click', () => {
+//   modal.classList.remove('hidden');
+// });
+
+// // Закрити модалку при кліку на кнопку
+// closeModalBtn.addEventListener('click', () => {
+//   modal.classList.add('hidden');
+// });
+
+// // Закрити при кліку поза контентом
+// modal.addEventListener('click', event => {
+//   if (event.target === modal) {
+//     modal.classList.add('hidden');
+//   }
+// });
